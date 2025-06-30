aef787d633265e1743be852e9580c57ca0118c06
Malya-reddy <malyareddy646@gmail.com>
2025-06-30 20:38:43 +0530

    Add files via upload

--- Project Files/backend/models/User.js
+++ Project Files/backend/models/User.js
@@ -1,1 +1,47 @@
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,  // 👈 removed unique: true here
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    userType: {
+      type: String,
+      enum: ["owner", "renter"],
+      default: "renter",
+    },
+    profileImagePath: {
+      type: String,
+      default: "",
+    },
+    tripList: {
+      type: Array,
+      default: [],
+    },
+    wishList: {
+      type: Array,
+      default: [],
+    },
+    propertyList: {
+      type: Array,
+      default: [],
+    },
+    reservationList: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
